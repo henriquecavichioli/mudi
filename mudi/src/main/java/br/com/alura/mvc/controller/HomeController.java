c93d09d0c74dc0a963065efacd10ca2eb283a556
Henrique <henriqueemann@gmail.com>
2022-06-28 23:02:47 -0300

    injecao de dependencias e utilizacao da derived query

--- mudi/src/main/java/br/com/alura/mvc/controller/HomeController.java
+++ mudi/src/main/java/br/com/alura/mvc/controller/HomeController.java
@@ -1,31 +1,26 @@
 package br.com.alura.mvc.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.alura.mvc.model.Pedido;
+import br.com.alura.mvc.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
 	
-	@PersistenceContext
-	private EntityManager entityManager;
+	@Autowired
+	private PedidoRepository pedidoRepository;
 	
 	@GetMapping("/home")
 	public String home(Model model) {
 		
-		Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
-		List<Pedido> listaPedidos = query.getResultList();
-		
-		model.addAttribute("pedidos", listaPedidos);
+		List<Pedido> pedidos = pedidoRepository.findAll();
+		model.addAttribute("pedidos", pedidos);
 		return "home";
 		
 	}
